512c5e885b8ce5ac0a6f725ac239066ff3621f62
Idan Zohar <darkyyy@gmail.com>
2024-09-30 22:02:30 +0300

    reversion

--- main.py
+++ main.py
@@ -1,29 +1,66 @@
-import json,requests,random
-from flask import Flask
-from flask import render_template
+import json,requests,random,os.path
+from flask import Flask,render_template
+
+BASE_API_URL = "https://pokeapi.co/api/v2/pokemon"
+POKEMON_LIMIT = 151
+CACHE_FILE = 'cache.json'
+
+def fetch_pokemon_data(url):
+    response = requests.get(url)
+    return response.json()
+
+def get_random_pokemon():
+    url = f"{BASE_API_URL}?limit={POKEMON_LIMIT}"
+    pokemon_list = fetch_pokemon_data(url)["results"]
+    random_pokemon = random.choice(pokemon_list)
+    return fetch_pokemon_data(random_pokemon["url"])
+
+def load_cache():
+    if os.path.exists(CACHE_FILE):
+        with open(CACHE_FILE, 'r') as file:
+            try:
+                return json.load(file)
+            except json.JSONDecodeError:
+                return {}
+    else:
+        return {}
+
+def save_to_cache(pokemon_data):
+    cache = load_cache()
+    pokemon_name = pokemon_data['name']
+    if pokemon_name not in cache:
+        cache[pokemon_name] = pokemon_data
+        
+        with open(CACHE_FILE, 'w') as file:
+            json.dump(cache, file, indent=4)
+
+def extract_pokemon_info(pokemon_data):
+    pokemon_name = pokemon_data['name']
+    pokemon_img = pokemon_data['sprites']['front_default']
+    pokemon_types = pokemon_data['types'][0]['type']['name']
+
+    return {
+        "name": pokemon_name,
+        "img": pokemon_img,
+        "types": pokemon_types
+    }
+
 # creates a Flask application
 app = Flask(__name__)
-def get_pokemon():
-    POKEMON_API = "https://pokeapi.co/api/v2/pokemon"
-    LIMIT = 5
-    url = f"{POKEMON_API}?limit={LIMIT}"
-    url2 = f"{POKEMON_API}"
-    response = requests.get(url2)
-    data = response.json()
-    print(data)
 
-    response = requests.get(url)
-    data = response.json()
-    return render_template('index.html',message=data)
 @app.route("/")
-def serve_pokemon():
-    data = "hello"
-    return render_template('index.html',message=data)
-
-@app.route("/random")
 def serve_random_pokemon():
-    data = "random pokemon"
-    return render_template('index.html',message=data)
+    cache = load_cache()
+    pokemon_data = get_random_pokemon()
+    pokemon_info = extract_pokemon_info(pokemon_data)
+
+    pokemon_name = pokemon_info['name']
+    if pokemon_name in cache:
+        pokemon_info = cache[pokemon_name]
+    else:
+        save_to_cache(pokemon_info)
+    return render_template('index.html', poke_name=pokemon_info['name'].capitalize(),poke_img=pokemon_info['img'],poke_type=pokemon_info['types'].capitalize())
 
 if __name__ == "__main__":
     app.run()
+
--- static/script.js
+++ static/script.js
@@ -1,1 +1,15 @@
-console.log('script loaded')
+if (document.getElementById("ptype").textContent.toLowerCase() === "fire") {
+    document.getElementById("card").style.backgroundColor = "rgba(150, 0, 0, 0.2)";
+}
+
+if (document.getElementById("ptype").textContent.toLowerCase() === "grass") {
+    document.getElementById("card").style.backgroundColor = "rgba(68, 218, 100, 0.2)";
+}
+
+if (document.getElementById("ptype").textContent.toLowerCase() === "water") {
+    document.getElementById("card").style.backgroundColor = "rgba(72, 156, 212, 0.2)";
+}
+
+if (document.getElementById("ptype").textContent.toLowerCase() === "electric") {
+    document.getElementById("card").style.backgroundColor = "rgba(255,231,0,0.2)";
+}
